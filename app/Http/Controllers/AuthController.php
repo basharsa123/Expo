d7c5b08855d67e915dabb563407388066d6bfdfe
bashar_sawas <basharsawas5219348@gamil.com>
2025-06-08 01:55:54 +0300

    building project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthController extends Controller
+{
+    use HasApiTokens;
+    // Register a new user
+    public function register(Request $request)
+    {
+       try{
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:users',
+                'password' => 'required|string|min:6|confirmed',
+            ], [
+                "name.required" => "Name is required",
+                "email.required" => "Email is required",
+                "password.required" => "Password is required",
+                "password.min" => "Password must be at least 8 characters",
+                "email.unique" => "Email already exists",
+                "email.email" => "should be an email",
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+
+            $token = $user->createToken('RegisterToken')->plainTextToken;
+
+            return response()->json([
+                'message' => 'User registered successfully',
+                'token' => $token,
+                'user' => new  UserResource($user),
+            ],201);
+        }catch(\Exception $e)
+        {
+            return response()->json(["error" => $e->getMessage()],500);
+        }
+    }
+
+    // Login existing user
+    public function login(Request $request)
+    {
+        try{
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ],
+            [
+                "email.required" => "Email is required",
+                "password.required" => "Password is required",
+            ]);
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $user->createToken('LoginToken')->plainTextToken,
+                'user' => new UserResource($user),
+            ],201);
+        }catch(\Exception $e)
+        {
+            return response()->json(["error" => $e->getMessage()],500);
+        }
+    }
+
+    // Logout the current session/token
+    public function logout(Request $request)
+    {
+
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return response()->json(['message' => 'Logged out successfully']);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Logout failed',
+                'error'   => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    // Show authenticated user with token
+    public function show(Request $request)
+    {
+        try{
+            return response()->json([
+                'token' => $request->bearerToken(),
+                'user' => new UserResource($request->user()),
+            ]);
+        }catch(\Exception $e)
+        {
+            return response()->json(["error" => $e->getMessage()] , 500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+//? Sanctum authentication system =_=
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+    return ['token' => $token->plainTextToken];
+});
+
+
+Route::group(
+[
+    "middleware" => "api",
+    "prefix" => "auth"
+], function ()
+{
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/show', [AuthController::class, 'show'])->middleware('auth:sanctum'); // middleware is too important
+    Route::delete('/logout', [AuthController::class, 'logout'])->middleware("auth:sanctum"); // middleware is too important
+});
